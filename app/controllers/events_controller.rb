2dbcd5f886d8b56b823c562a7de50ba1c3de88e9
Overlander <anuragnandal@hotmail.com>
2022-08-22 12:18:21 -0400

    Add #post_params, and add #new and #create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.create(post_params)
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:event).permit(:name, :date)
+  end
 end
