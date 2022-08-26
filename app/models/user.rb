e7ff8bbface02c9dbf516983c4a456d683602d6c
Overlander <anuragnandal@hotmail.com>
2022-08-26 08:13:08 -0400

    Modify and fix attended_events association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :invitations, foreign_key: :attendee_id
-  has_many :attended_events, through: :invitations
+  has_many :attended_events, through: :invitations, source: :attendee
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
