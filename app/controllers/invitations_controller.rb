9a233af7130d46d938cac554dab86cc8fdf993a8
Overlander <anuragnandal@hotmail.com>
2022-08-26 08:12:22 -0400

    Add new and create actions

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,21 @@
 class InvitationsController < ApplicationController
+  def new
+    @invitation = Invitation.new
+  end
+
+  def create
+    @invitation = current_user.invitations.build(invitation_params)
+
+    if @invitation.save!
+      redirect_to :back,
+      notice: "You've been added to this event's list of attendees."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def invitation_params
+    params.require(:invitations).permit(:attendee_id, :created_event_id)
+  end
 end
