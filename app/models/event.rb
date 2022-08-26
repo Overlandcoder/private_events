5b83c116e36002ec8acfdf9b3e00e8b0dacbe59e
Overlander <anuragnandal@hotmail.com>
2022-08-26 17:09:49 -0400

    Replace class methods with scopes for past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :invitations, source: :attendee
   belongs_to :creator, class_name: "User"
 
-  def self.in_the_past
-    self.where("Date < ?", Date.current)
-  end
-
-  def self.in_the_future
-    self.where("Date > ?", Date.current)
-  end
+  scope :in_the_past, -> { where("date < ?", Date.current) }
+  scope :in_the_future, -> { where("date > ?", Date.current) }
 end
