2126e69651fe113ab2bcf75c0b33fe8ce80b19d8
Overlander <anuragnandal@hotmail.com>
2022-08-26 16:49:59 -0400

    Create class methods that return past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :invitations, foreign_key: :created_event_id
   has_many :attendees, through: :invitations, source: :attendee
   belongs_to :creator, class_name: "User"
+
+  def self.in_the_past
+    self.where("Date < ?", Date.current)
+  end
+
+  def self.in_the_future
+    self.where("Date > ?", Date.current)
+  end
 end
